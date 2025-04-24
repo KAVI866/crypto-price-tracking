b695d76c160c87f91d738b0970061d7c550790ae
KAVIYARASAN M <kaviyarasan1029@gmail.com>
2025-04-24 15:21:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-app/', // <-- use your repo name here
+  base: '/crypto-price-tracking/', // <-- use your repo name here
   plugins: [react()],
 })
